caeb2a27f5ecf108480e22021ca2a57d8bf3b207
reejung.kim <reejung.kim@gmail.com>
2022-11-12 00:35:19 +0900

    Edit weights dir

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -184,8 +184,8 @@ def object_detection_image():
         #f = open(r'C:\Users\Olazaah\Downloads\stream\labels\coconames.txt','r')
         #lines = f.readlines()
         #classNames = [line.strip() for line in lines]
-        config_path =  'yolov5s.pt' #'yolov3.cfg'
-        weights_path = 'yolov3.weights'
+        config_path =  'yolov3.cfg'
+        weights_path = 'yolov5s.pt' #'yolov3.weights'
         net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) #CPU로 세팅
