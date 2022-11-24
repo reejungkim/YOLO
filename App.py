742a9e9dda183850879aa9c30f9055fe8f3ac8e3
reejung.kim <reejung.kim@gmail.com>
2022-11-24 22:27:06 +0900

    Add weight reader

--- App.py
+++ App.py
@@ -186,7 +186,7 @@ def object_detection_image():
         #lines = f.readlines()
         #classNames = [line.strip() for line in lines]
         config_path =  'yolov3.cfg'
-        weights_path = 'wget https://pjreddie.com/media/files/yolov3.weights' #'yolov3.weights'
+        weights_path = WeightReader('https://pjreddie.com/media/files/yolov3.weights') #'yolov3.weights'
         net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU) #CPU로 세팅
@@ -243,6 +243,57 @@ def object_detection_image():
 
         #cv2.destroyAllWindows()
         my_bar.progress(100)
+        
+
+class WeightReader:
+	def __init__(self, weight_file):
+		with open(weight_file, 'rb') as w_f:
+			major,	= struct.unpack('i', w_f.read(4))
+			minor,	= struct.unpack('i', w_f.read(4))
+			revision, = struct.unpack('i', w_f.read(4))
+			if (major*10 + minor) >= 2 and major < 1000 and minor < 1000:
+				w_f.read(8)
+			else:
+				w_f.read(4)
+			transpose = (major > 1000) or (minor > 1000)
+			binary = w_f.read()
+		self.offset = 0
+		self.all_weights = np.frombuffer(binary, dtype='float32')
+ 
+	def read_bytes(self, size):
+		self.offset = self.offset + size
+		return self.all_weights[self.offset-size:self.offset]
+ 
+	def load_weights(self, model):
+		for i in range(106):
+			try:
+				conv_layer = model.get_layer('conv_' + str(i))
+				print("loading weights of convolution #" + str(i))
+				if i not in [81, 93, 105]:
+					norm_layer = model.get_layer('bnorm_' + str(i))
+					size = np.prod(norm_layer.get_weights()[0].shape)
+					beta  = self.read_bytes(size) # bias
+					gamma = self.read_bytes(size) # scale
+					mean  = self.read_bytes(size) # mean
+					var   = self.read_bytes(size) # variance
+					weights = norm_layer.set_weights([gamma, beta, mean, var])
+				if len(conv_layer.get_weights()) > 1:
+					bias   = self.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+					kernel = self.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+					kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+					kernel = kernel.transpose([2,3,1,0])
+					conv_layer.set_weights([kernel, bias])
+				else:
+					kernel = self.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+					kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+					kernel = kernel.transpose([2,3,1,0])
+					conv_layer.set_weights([kernel])
+			except ValueError:
+				print("no convolution #" + str(i))
+ 
+	def reset(self):
+		self.offset = 0
+ 
 
 
 def main():
