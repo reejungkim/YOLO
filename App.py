0b9e68db56114e928d2b8b200e69808c9ff228ef
reejung.kim <reejung.kim@gmail.com>
2023-02-25 14:09:55 +0900

    Add mediapipe

--- App.py
+++ App.py
@@ -17,7 +17,7 @@ import time
 import sys
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 import av 
-#import mediapipe as mp
+import mediapipe as mp
 import queue 
 
 
@@ -149,7 +149,7 @@ def object_detection_video():
                         # now put the text (label: confidence %)
                         cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
                                     fontScale=font_scale, color=(0, 0, 0), thickness=thickness)
-
+                
                 out.write(image)
                 st.image(image, caption='Proccesed Video.')
                 #cv2.imshow("image", image)
@@ -168,14 +168,44 @@ def object_detection_video():
 #Reading from WebCam
 def obejct_detection_webcam():
 
-    
-    RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    )
+    def process(image):
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+    # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+        return cv2.flip(image, 1)
+    RTC_CONFIGURATION = RTCConfiguration(
+        {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    )
+    class VideoProcessor:
+        def recv(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+    img = process(img)
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
     webrtc_ctx = webrtc_streamer(
-        key="object-detection",
+        key="WYH",
         mode=WebRtcMode.SENDRECV,
         rtc_configuration=RTC_CONFIGURATION,
-        #video_frame_callback=callback,
         media_stream_constraints={"video": True, "audio": False},
+        video_processor_factory=VideoProcessor,
         async_processing=True,
     )
 
